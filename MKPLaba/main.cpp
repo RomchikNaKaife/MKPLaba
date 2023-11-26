2dd7b78bc8cd48defb1c136cb1f7f026237d525d
Roman <roman.spam2004@yaandex.ru>
2023-11-26 23:24:10 +0300

    Initial Commit

---
+++ MKPLaba/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MKPLaba
+//
+//  Created by Роман Перепонов on 26.11.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
